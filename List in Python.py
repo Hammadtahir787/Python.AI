1aadc3dcfbf6ae7712aac8a63c5262ebe0e6f396
Hammadtahir787 <hammadtahir787@gmail.com>
2024-10-31 09:26:26 +0500

    Add files via upload

---
+++ List in Python.py
@@ -0,0 +1,10 @@
+promo=[21,"Hammad",5.6,"karachi"]
+print(promo[2])
+print(promo[1])
+promo[1]="Suleman"
+print(promo[1])
+
+
+#Creating a list of Numbers
+List=[10,20,14]
+print(f"The List of numbers are:\n{List}")
