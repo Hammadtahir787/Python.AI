d2c89f79f2e17b241a9e939e30a8528b94239f7d
Hammadtahir787 <hammadtahir787@gmail.com>
2025-01-01 21:32:16 -0800

    Linear Regression.py

---
+++ Linear Regression.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {'Name':['Alice','Bob','charlie'],
+        'Age':[20,25,35],
+        'city':['new york','Los angeles','chicago']}
+
+df = pd.DataFrame(data)
+
+print(df)
